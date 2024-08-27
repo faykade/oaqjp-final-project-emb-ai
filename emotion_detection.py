56571128f1a8646288787303a1a68c996a9ef5e6
= <ulrthomas@gmail.com>
2024-08-27 17:49:56 -0400

    finishes up the initial emotion detection stage

--- emotion_detection.py
+++ emotion_detection.py
@@ -17,13 +17,31 @@ def emotion_detector(text_to_analyze):
             "text": text_to_analyze 
         }
     }
-    res = requests.post(url = url, json = body, headers = headers, timeout = 5000)
+    response = requests.post(url = url, json = body, headers = headers, timeout = 5000)
     
+    # tracked attributes
     keys = ('anger', 'disgust', 'fear', 'joy', 'sadness')
+    # store current maximum key/val
+    max_val = (None, 0)
+    # dict to store emotion values
+    emotions = {}
 
+    # only parse if everything is successful
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotion_predictions = formatted_response.get('emotionPredictions')
+        if emotion_predictions and len(emotion_predictions):
+            res_emotions = emotion_predictions[0].get('emotion')
+            # for every tracked attribute, set a new max if necessary, and store the emotion value
+            for key in keys:
+                res_emotion_val = res_emotions.get(key)
+                emotions[key] = res_emotion_val
+                if res_emotion_val > max_val[1]:
+                    max_val = (key, res_emotion_val)
+    
+    res = {}
     for key in keys:
-        
+        res[key] = emotions.get(key)
+    res['dominant_emotion'] = max_val[0]
 
-
-
-    return res.text
+    return res
